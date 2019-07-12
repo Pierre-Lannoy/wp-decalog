7fd032373a001e0e9a3a4cb89a760d1e16b3125b
Pierre Lannoy <pierre.lannoy@adeo.com>
2019-07-12 14:55:20 +0200

    Warning in the "about" page when dev or rc version.

--- admin/partials/decalog-admin-settings-about.php
+++ admin/partials/decalog-admin-settings-about.php
@@ -9,6 +9,16 @@
  * @since   1.0.0
  */
 
+use Decalog\System\Environment;
+
+$warning = '';
+if (Environment::is_plugin_in_dev_mode()) {
+	$warning = '<p>⚠️&nbsp;' . sprintf(esc_html__('This version of %s is not production-ready. It is a development preview. Use it at your own risk!', 'decalog'), DECALOG_PRODUCT_NAME) . '</p>';
+}
+if (Environment::is_plugin_in_rc_mode()) {
+	$warning = '<p>⚠️&nbsp;' . sprintf(esc_html__('This version of %s is a release candidate. Although ready for production, this version is not officially supported in production environments.', 'decalog'), DECALOG_PRODUCT_NAME) . '</p>';
+}
+
 $intro      = sprintf( esc_html__( '%1$s is a free and open source plugin for WordPress. It integrates other free and open source works (as-is or modified) like: %2$s.', 'decalog' ), '<em>' . DECALOG_PRODUCT_NAME . '</em>', do_shortcode( '[decalog-libraries]' ) );
 $trademarks = __( 'All brands, icons and graphic illustrations are registered trademarks of their respective owners.', 'decalog' );
 $brands     = array( 'Automattic' );
@@ -16,6 +26,7 @@ $official   = sprintf( __( 'This plugin is not an official software from %s and,
 
 ?>
 <h2><?php echo esc_html( DECALOG_PRODUCT_NAME . ' ' . DECALOG_VERSION ); ?></h2>
+<?php echo $warning; ?>
 <p><?php echo $intro; ?></p>
 <h4><?php esc_html_e( 'Disclaimer', 'decalog' ); ?></h4>
 <p><?php echo esc_html( $official ); ?></p>
