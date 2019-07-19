d2dec1e496c910180ecf3b3417414f7733f48464
Pierre Lannoy <pierre.lannoy@adeo.com>
2019-07-19 10:41:59 +0200

    Stub for Monolog 'WordpressHandler'.

---
+++ includes/handlers/class-wordpresshandler.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * WordPress handler for Monolog
+ *
+ * Handles all features of WordPress handler for Monolog.
+ *
+ * @package Handlers
+ * @author  Pierre Lannoy <https://pierre.lannoy.fr/>.
+ * @since   1.0.0
+ */
+
+namespace Decalog\Handler;
+
+use Monolog\Logger;
+use Monolog\Handler\AbstractProcessingHandler;
+
+/**
+ * Define the Monolog WordPress handler.
+ *
+ * Handles all features of WordPress handler for Monolog.
+ *
+ * @package Handlers
+ * @author  Pierre Lannoy <https://pierre.lannoy.fr/>.
+ * @since   1.0.0
+ */
+class WordpressHandler extends AbstractProcessingHandler {
+
+	/**
+	 * The table name.
+	 *
+	 * @since  1.0.0
+	 * @var    string    $table    The table name.
+	 */
+	private $table = '';
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @param   string  $table    The table name.
+	 * @param   integer $level    Optional. The min level to log.
+	 * @param   boolean $bubble   Optional. Has the record to bubble?
+	 * @since    1.0.0
+	 */
+	public function __construct( string $table, $level = Logger::DEBUG, bool $bubble = true ) {
+		parent::__construct( $level, $bubble );
+		$this->table = $table;
+	}
+
+	/**
+	 * Write the record in the table.
+	 *
+	 * @param   array $record    The record to write.
+	 * @since    1.0.0
+	 */
+	protected function write( array $record ): void {
+
+
+	}
+}
