7542325ffb6f55eb62dfe95d5b7b4f5d4bda459f
Pierre Lannoy <pierre.lannoy@adeo.com>
2019-10-01 08:37:17 +0200

    Verify if an IP is not private or reserved before trying to get host name.

--- includes/system/class-http.php
+++ includes/system/class-http.php
@@ -191,13 +191,22 @@ class Http {
 	/**
 	 * Get the top domain of a full fqdn host.
 	 *
-	 * @param string $host The host.
+	 * @param string  $host     The host.
+	 * @param boolean $resolve  Optional. Try to resolve the host name.
 	 * @return  string  The top domain.
 	 * @since  1.0.0
 	 */
-	public static function top_domain( $host ) {
+	public static function top_domain( $host, $resolve = true ) {
 		if ( filter_var( $host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6 | FILTER_FLAG_IPV4 ) ) {
-			return $host;
+			if ( $resolve ) {
+				if ( filter_var( $host, FILTER_VALIDATE_IP, FILTER_FLAG_NO_RES_RANGE | FILTER_FLAG_NO_PRIV_RANGE ) ) {
+					return self::top_domain( gethostbyaddr( $host ), false );
+				} else {
+					return $host;
+				}
+			} else {
+				return $host;
+			}
 		}
 		$parts       = explode( '.', $host );
 		$middle_part = array_slice( $parts, -2, 1 );
