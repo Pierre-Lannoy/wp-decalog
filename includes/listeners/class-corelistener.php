a7df770a7af8566f32da74164697cfb0e412b064
Pierre Lannoy <pierre.lannoy@adeo.com>
2019-11-08 11:41:26 +0100

    Fix for [PRV001].

--- includes/listeners/class-corelistener.php
+++ includes/listeners/class-corelistener.php
@@ -544,7 +544,7 @@ class CoreListener extends AbstractListener {
 			$name = 'somebody';
 		}
 		if ( isset( $this->logger ) ) {
-			$this->logger->notice( sprintf( 'Failed login for "%s".', $username ) );
+			$this->logger->notice( sprintf( 'Failed login for "%s".', $name ) );
 		}
 	}
 
