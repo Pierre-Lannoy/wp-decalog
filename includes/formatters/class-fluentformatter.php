27e2d192f9329a2db8cadb91beb274dd4482c811
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-03-08 19:13:16 +0100

    Restores old fluent formatter, as there's now a new formatter specific to stackdriver.

--- includes/formatters/class-fluentformatter.php
+++ includes/formatters/class-fluentformatter.php
@@ -41,62 +41,11 @@ class FluentFormatter implements FormatterInterface {
 		}
 		$tag     = strtolower( DECALOG_PRODUCT_SHORTNAME . '.' . $record['channel'] . '.' . $level );
 		$message = [
-			'severity' => $level,
-			'message'  => $record['message'],
-			'context'  => $record['context'],
-			'extra'    => $record['extra'],
-		];
-		/*if ( array_key_exists( 'file', $record['extra'] ) && $record['extra']['file'] && is_string( $record['extra']['file'] ) ) {
-			$message['logging.googleapis.com/sourceLocation']['file'] = $record['extra']['file'];
-		}
-		if ( array_key_exists( 'line', $record['extra'] ) && $record['extra']['line'] ) {
-			$message['logging.googleapis.com/sourceLocation']['line'] = (int) $record['extra']['line'];
-		}
-		if ( array_key_exists( 'function', $record['extra'] ) && $record['extra']['function'] && is_string( $record['extra']['function'] ) ) {
-			$message['logging.googleapis.com/sourceLocation']['function'] = $record['extra']['function'];
-			if ( array_key_exists( 'class', $record['extra'] ) && $record['extra']['class'] && is_string( $record['extra']['class'] ) ) {
-				$message['logging.googleapis.com/sourceLocation']['function'] = $record['extra']['class'] . '::' . $message['logging.googleapis.com/sourceLocation']['function'];
-			}
-		}
-		if ( array_key_exists( 'ip', $record['extra'] ) && is_string( $record['extra']['ip'] ) ) {
-			$message['httpRequest']['remoteIp'] = $record['extra']['ip'];
-		}
-		if ( array_key_exists( 'url', $record['extra'] ) && is_string( $record['extra']['url'] ) ) {
-			$message['httpRequest']['requestUrl'] = $record['extra']['url'];
-		}
-		if ( array_key_exists( 'http_method', $record['extra'] ) && is_string( $record['extra']['http_method'] ) ) {
-			if ( in_array( strtolower( $record['extra']['http_method'] ), Http::$verbs, true ) ) {
-				$message['httpRequest']['requestMethod'] = strtoupper( $record['extra']['http_method'] );
-			}
-		}
-		if ( array_key_exists( 'code', $record['context'] ) && is_numeric( $record['context']['code'] ) ) {
-			if ( 0 < (int) $record['context']['code'] && array_key_exists( (int) $record['context']['code'], Http::$http_status_codes ) ) {
-				$message['httpRequest']['status'] = (int) $record['context']['code'];
-			}
-		}
-		if ( array_key_exists( 'referrer', $record['extra'] ) && is_string( $record['extra']['referrer'] ) ) {
-			$message['httpRequest']['referrer'] = $record['extra']['referrer'];
-		}
-		if ( array_key_exists( 'ua', $record['extra'] ) && is_string( $record['extra']['ua'] ) ) {
-			$message['httpRequest']['userAgent'] = $record['extra']['ua'];
-		}*/
-		//error_log(wp_json_encode( [ $tag, $record['datetime']->getTimestamp(), $message ] ));
-		return json_encode( [ $tag, $record['datetime']->getTimestamp(), $message ] );
-
-
-
-		/*if ( array_key_exists( $record['level'], EventTypes::$level_names ) ) {
-			$level = EventTypes::$level_names[ $record['level'] ];
-		} else {
-			$level = 'UNKNOWN';
-		}
-		$tag     = strtolower( DECALOG_PRODUCT_SHORTNAME . '.' . $record['channel'] . '.' . $level );
-		$message = [
 			'message' => $record['message'],
 			'context' => $record['context'],
 			'extra'   => $record['extra'],
 		];
-		return wp_json_encode( [ $tag, $record['datetime']->getTimestamp(), $message ] );*/
+		return wp_json_encode( [ $tag, $record['datetime']->getTimestamp(), $message ] );
 	}
 	/**
 	 * Formats a set of log records.
