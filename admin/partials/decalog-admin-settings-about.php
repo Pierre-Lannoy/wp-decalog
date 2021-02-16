9f4001f04818ab5712b21513b7d0fda9c814a69a
Pierre Lannoy <pierre@lannoy.fr>
2021-02-16 11:23:11 +0100

    Adds Raygun to the editor list.

--- admin/partials/decalog-admin-settings-about.php
+++ admin/partials/decalog-admin-settings-about.php
@@ -26,7 +26,7 @@ if ( Environment::is_plugin_in_rc_mode() ) {
 $icon       = '<img class="decalog-about-logo" style="opacity:0;" src="' . Decalog\Plugin\Core::get_base64_logo() . '" />';
 $intro      = sprintf( esc_html__( '%1$s is a free and open source plugin for WordPress. It integrates other free and open source works (as-is or modified) like: %2$s.', 'decalog' ), '<em>' . DECALOG_PRODUCT_NAME . '</em>', do_shortcode( '[decalog-libraries]' ) );
 $trademarks = esc_html__( 'All brands, icons and graphic illustrations are registered trademarks of their respective owners.', 'decalog' );
-$brands     = array( 'Automattic', 'Bugsnag', 'Elastic Cloud', 'Fluentd Project', 'Google', 'Pushover', 'Rapid7', 'Sematext', 'Slack', 'Solarwinds', 'Sumo Logic' );
+$brands     = array( 'Automattic', 'Bugsnag', 'Elastic Cloud', 'Fluentd Project', 'Google', 'Pushover', 'Rapid7', 'Raygun', 'Sematext', 'Slack', 'Solarwinds', 'Sumo Logic' );
 $official   = sprintf( esc_html__( 'This plugin is not an official software from %s and, as such, is not endorsed or supported by these companies.', 'decalog' ), implode( ', ', $brands ) );
 
 ?>
