0ab3e82a2b745969b1d9290e93e6172707bbb5f0
Pierre Lannoy <pierre@lannoy.fr>
2021-05-04 23:55:56 +0200

    Dissociates processors for tracing.

--- includes/features/class-processortypes.php
+++ includes/features/class-processortypes.php
@@ -33,63 +33,112 @@ class ProcessorTypes {
 	/**
 	 * Initialize the class and set its properties.
 	 *
+	 * @param   boolean Optional. Are the processors for a traces logger?.
 	 * @since    1.0.0
 	 */
-	public function __construct() {
-		$this->processors[] = [
-			'id'        => 'BacktraceProcessor',
-			'namespace' => 'Decalog\\Processor',
-			'name'      => esc_html__( 'Backtrace', 'decalog' ),
-			'help'      => esc_html__( 'Allows to log the full PHP and WordPress call stack.', 'decalog' ),
-			'init'      => [
-				[ 'type' => 'level' ],
-			],
-		];
-		$this->processors[] = [
-			'id'        => 'IntrospectionProcessor',
-			'namespace' => 'Decalog\\Processor',
-			'name'      => esc_html__( 'PHP introspection', 'decalog' ),
-			'help'      => esc_html__( 'Allows to log line, file, class and function generating the event.', 'decalog' ),
-			'init'      => [
-				[ 'type' => 'level' ],
-			],
-		];
-		$this->processors[] = [
-			'id'        => 'WWWProcessor',
-			'namespace' => 'Decalog\\Processor',
-			'name'      => esc_html__( 'HTTP request', 'decalog' ),
-			'help'      => esc_html__( 'Allows to log url, method, referrer and remote IP of the current web request.', 'decalog' ),
-			'init'      => [
-				[
-					'type'  => 'literal',
-					'value' => null,
+	public function __construct( $tracing = false ) {
+		if ( $tracing ) {
+			$this->processors[] = [
+				'id'        => 'IntrospectionProcessor',
+				'namespace' => 'Decalog\\Processor',
+				'name'      => esc_html__( 'PHP introspection', 'decalog' ),
+				'help'      => esc_html__( 'Allows to log line, file, class and function where the span starts.', 'decalog' ),
+				'init'      => [
+					[ 'type' => 'level' ],
 				],
-				[
-					'type'  => 'literal',
-					'value' => null,
+			];
+			$this->processors[] = [
+				'id'        => 'WWWProcessor',
+				'namespace' => 'Decalog\\Processor',
+				'name'      => esc_html__( 'HTTP request', 'decalog' ),
+				'help'      => esc_html__( 'Allows to log url, method, referrer and remote IP of the current web request.', 'decalog' ),
+				'init'      => [
+					[
+						'type'  => 'literal',
+						'value' => null,
+					],
+					[
+						'type'  => 'literal',
+						'value' => null,
+					],
+					[
+						'type'  => 'privacy',
+						'value' => 'obfuscation',
+					],
 				],
-				[
-					'type'  => 'privacy',
-					'value' => 'obfuscation',
+			];
+			$this->processors[] = [
+				'id'        => 'WordpressProcessor',
+				'namespace' => 'Decalog\\Processor',
+				'name'      => esc_html__( 'WordPress', 'decalog' ),
+				'help'      => esc_html__( 'Allows to log site, user and remote IP of the current request.', 'decalog' ),
+				'init'      => [
+					[
+						'type'  => 'privacy',
+						'value' => 'pseudonymization',
+					],
+					[
+						'type'  => 'privacy',
+						'value' => 'obfuscation',
+					],
 				],
-			],
-		];
-		$this->processors[] = [
-			'id'        => 'WordpressProcessor',
-			'namespace' => 'Decalog\\Processor',
-			'name'      => esc_html__( 'WordPress', 'decalog' ),
-			'help'      => esc_html__( 'Allows to log site, user and remote IP of the current request.', 'decalog' ),
-			'init'      => [
-				[
-					'type'  => 'privacy',
-					'value' => 'pseudonymization',
+			];
+		} else {
+			$this->processors[] = [
+				'id'        => 'BacktraceProcessor',
+				'namespace' => 'Decalog\\Processor',
+				'name'      => esc_html__( 'Backtrace', 'decalog' ),
+				'help'      => esc_html__( 'Allows to log the full PHP and WordPress call stack.', 'decalog' ),
+				'init'      => [
+					[ 'type' => 'level' ],
 				],
-				[
-					'type'  => 'privacy',
-					'value' => 'obfuscation',
+			];
+			$this->processors[] = [
+				'id'        => 'IntrospectionProcessor',
+				'namespace' => 'Decalog\\Processor',
+				'name'      => esc_html__( 'PHP introspection', 'decalog' ),
+				'help'      => esc_html__( 'Allows to log line, file, class and function generating the event.', 'decalog' ),
+				'init'      => [
+					[ 'type' => 'level' ],
 				],
-			],
-		];
+			];
+			$this->processors[] = [
+				'id'        => 'WWWProcessor',
+				'namespace' => 'Decalog\\Processor',
+				'name'      => esc_html__( 'HTTP request', 'decalog' ),
+				'help'      => esc_html__( 'Allows to log url, method, referrer and remote IP of the current web request.', 'decalog' ),
+				'init'      => [
+					[
+						'type'  => 'literal',
+						'value' => null,
+					],
+					[
+						'type'  => 'literal',
+						'value' => null,
+					],
+					[
+						'type'  => 'privacy',
+						'value' => 'obfuscation',
+					],
+				],
+			];
+			$this->processors[] = [
+				'id'        => 'WordpressProcessor',
+				'namespace' => 'Decalog\\Processor',
+				'name'      => esc_html__( 'WordPress', 'decalog' ),
+				'help'      => esc_html__( 'Allows to log site, user and remote IP of the current request.', 'decalog' ),
+				'init'      => [
+					[
+						'type'  => 'privacy',
+						'value' => 'pseudonymization',
+					],
+					[
+						'type'  => 'privacy',
+						'value' => 'obfuscation',
+					],
+				],
+			];
+		}
 	}
 
 	/**
