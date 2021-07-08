835fb15abc30f4f3c2a43062ed5c9fe24ee86aca
Pierre Lannoy <pierre@lannoy.fr>
2021-07-08 19:39:57 +0200

    Adapts names.

--- perfopsone/init.php
+++ perfopsone/init.php
@@ -8,9 +8,9 @@
  */
 
 if ( ! defined( 'PERFOO_INITIALIZED' ) ) {
-	define( 'PERFOO_PRODUCT_NAME', 'PerfOps-One' );
+	define( 'PERFOO_PRODUCT_NAME', 'PerfOps One' );
 	define( 'PERFOO_PRODUCT_URL', 'https://perfops.one' );
-	define( 'PERFOO_PRODUCT_SHORTNAME', 'PerfOps-One' );
+	define( 'PERFOO_PRODUCT_SHORTNAME', 'PerfOps One' );
 	require_once __DIR__ . '/autoload.php';
 	define( 'PERFOO_INITIALIZED', true );
 }
