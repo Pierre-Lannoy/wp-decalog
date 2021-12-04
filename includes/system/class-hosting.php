f7b555d834574bf2f12c52f39a1ce5cec33a07e9
Pierre Lannoy <pierre@lannoy.fr>
2021-12-04 17:57:10 +0100

    Code cleaning.

--- includes/system/class-hosting.php
+++ includes/system/class-hosting.php
@@ -28,63 +28,43 @@ class Hosting {
 	}
 
 	/**
-	 * Check if the server config allows shell_exec().
+	 * Check if Cloudflare Geoip is enabled.
 	 *
-	 * @return bool    True if shell_exec() can be used, false otherwise.
+	 * @return bool    True if Cloudflare Geoip is enabled.
 	 * @since  1.0.0
 	 */
-	private static function is_shell_enabled() {
-		if ( function_exists( 'shell_exec' ) && ! in_array( 'shell_exec', array_map( 'trim', explode( ', ', ini_get( 'disable_functions' ) ) ), true ) && (int) strtolower( ini_get( 'safe_mode' ) ) !== 1 ) {
-			// phpcs:ignore
-			$return = shell_exec( 'cat /proc/cpuinfo' );
-			if ( ! empty( $return ) ) {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
+	public static function is_cloudflare_geoip_enabled() {
+		return array_key_exists( 'HTTP_CF_IPCOUNTRY', $_SERVER ) || array_key_exists( 'CF-IPCountry', $_SERVER );
 	}
 
 	/**
-	 * Get CPU count of the server.
+	 * Check if Cloudfront (AWS) Geoip is enabled.
 	 *
-	 * @return int|bool    The count of CPUs, false if it's not countable.
+	 * @return bool    True if Cloudfront Geoip is enabled.
 	 * @since  1.0.0
 	 */
-	public static function count_server_cpu() {
-		$cpu_count = Cache::get_global( '/Hardware/CPU/Count' );
-		if ( false === $cpu_count ) {
-			if ( self::is_shell_enabled() ) {
-				// phpcs:ignore
-				$cpu_count = shell_exec( 'cat /proc/cpuinfo |grep "physical id" | sort | uniq | wc -l' );
-				Cache::set_global( '/Hardware/CPU/Count', $cpu_count, 'diagnosis' );
-			} else {
-				return false;
-			}
-		}
-		return (int) $cpu_count;
+	public static function is_cloudfront_geoip_enabled() {
+		return array_key_exists( 'CloudFront-Viewer-Country', $_SERVER );
 	}
 
 	/**
-	 * Get core count of the server.
+	 * Check if Google LB Geoip is enabled.
 	 *
-	 * @return int|bool    The count of cores, false if it's not countable.
+	 * @return bool    True if Google Geoip is enabled.
+	 * @since  2.3.0
+	 */
+	public static function is_googlelb_geoip_enabled() {
+		return array_key_exists( 'X-Client-Geo-Location', $_SERVER );
+	}
+
+	/**
+	 * Check if Apache Geoip is enabled.
+	 *
+	 * @return bool    True if Cloudfront Geoip is enabled.
 	 * @since  1.0.0
 	 */
-	public static function count_server_core() {
-		$core_count = Cache::get_global( '/Hardware/Core/Count' );
-		if ( false === $core_count ) {
-			if ( self::is_shell_enabled() ) {
-				// phpcs:ignore
-				$core_count = shell_exec( "echo \"$((`cat /proc/cpuinfo | grep cores | grep -o '[0-9]' | uniq` * `cat /proc/cpuinfo |grep 'physical id' | sort | uniq | wc -l`))\"" );
-				Cache::set_global( '/Hardware/Core/Count', $core_count, 'diagnosis' );
-			} else {
-				return false;
-			}
-		}
-		return $core_count;
+	public static function is_apache_geoip_enabled() {
+		return array_key_exists( 'GEOIP_COUNTRY_CODE', $_SERVER );
 	}
 
 }
