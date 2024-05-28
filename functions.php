4e4affe2f8c2634d8fda7ebbb4fc74a424d8f1d6
Pierre Lannoy <pierre@hosterra.eu>
2024-05-28 08:32:48 +0200

    Adds Psr/Log version "detection".

--- functions.php
+++ functions.php
@@ -7,26 +7,36 @@
  * @since   2.0.0
  */
 
-/**
- * Get the loaded version of PSR-3.
- *
- * @return  int  The PSR-3 main version.
- * @since 4.0.0
- */
-function decalog_get_psr_log_version() {
-	if ( class_exists( '\Psr\Log\NullLogger' ) ) {
-		$reflection = new \ReflectionMethod( \Psr\Log\NullLogger::class, 'log');
-		foreach ( $reflection->getParameters() as $param ) {
-			if ( 'message' === $param->getName() ) {
-				if ( str_contains($param->getType() ?? '', '|') ) {
-					return 3;
-				}
-			}
+if ( ! function_exists('decalog_get_psr_log_version') ) {
+	/**
+	 * Get the needed version of PSR-3.
+	 *
+	 * @return  int  The PSR-3 needed version.
+	 * @since 4.0.0
+	 */
+	function decalog_get_psr_log_version() {
+		$required = 1;
+		if ( ! defined( 'DECALOG_PSR_LOG_VERSION') ) {
+			define( 'DECALOG_PSR_LOG_VERSION', 'V1' );
 		}
-	} else {
-		return 0;
+		switch ( strtolower( DECALOG_PSR_LOG_VERSION ) ) {
+			case 'v3':
+				$required = 3;
+				break;
+			case 'auto':
+				if ( class_exists( '\Psr\Log\NullLogger') ) {
+					$reflection = new \ReflectionMethod(\Psr\Log\NullLogger::class, 'log');
+					foreach ( $reflection->getParameters() as $param ) {
+						if ( 'message' === $param->getName() ) {
+							if ( str_contains($param->getType() ?? '', '|') ) {
+								$required = 3;
+							}
+						}
+					}
+				}
+		}
+		return $required;
 	}
-	return 1;
 }
 
 /**
